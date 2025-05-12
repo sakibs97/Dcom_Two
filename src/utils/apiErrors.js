9eb28d9b1308cce15b8b3b9a735f042cb45099b1
Sakib Hossain <sakibs.ngn@gmail.com>
2025-05-12 17:20:33 +0600

    Refactor ApiError and ApiSuccess classes; add additional static methods for error and success responses

--- src/utils/apiErrors.js
+++ src/utils/apiErrors.js
@@ -1,14 +1,41 @@
-class ApiError{
-    constructor(statusCode, message, errors, stack){
-        super(message)
-        this.success = false
-        this.statusCode= statusCode
-        this.message = message
-        this.errors = errors
-        if (stack) {
-            this.stack = stack
-        }else{
-            errors.captureStackTrace(this, this.constructor);
-        }
+class ApiError {
+  constructor(statusCode, message, errors, stack) {
+    super(message);
+    this.success = false;
+    this.statusCode = statusCode;
+    this.message = message;
+    this.errors = errors;
+    if (stack) {
+      this.stack = stack;
+    } else {
+      errors.captureStackTrace(this, this.constructor);
     }
+  }
+  static bdRequest(message = 'invalid request message framing', errors = []) {
+    return new ApiError(400, message, errors);
+  }
+  static unauthorized(
+    message = 'Although the HTTP standard specifies "unauthorized"',
+    errors = []
+  ) {
+    return new ApiError(401, message, errors);
+  }
+  static forbidden(message = 'The client does not have access rights to the content', errors = []) {
+    return new ApiError(403, message, errors);
+  }
+  static notFound(message = 'The server cannot find the requested resource.', errors = []) {
+    return new ApiError(404, message, errors);
+  }
+  static conflict(
+    message = 'This response is sent when a request conflicts with the current state of the server.',
+    errors = []
+  ) {
+    return new ApiError(409, message, errors);
+  }
+  static gone(message = 'content has been permanently deleted from server', errors = []) {
+    return new ApiError(410, message, errors);
+  }
+  static coustomError(statusCode, message = 'Error', errors = []) {
+    return new ApiError(statusCode, message, errors);
+  }
 }
--- src/utils/apiSuccess.js
+++ src/utils/apiSuccess.js
@@ -6,6 +6,27 @@ class ApiSuccess {
     this.message = message;
     this.data = data;
   }
+  static success(message = 'The request succeeded', data = {}) {
+    return new ApiSuccess(200, message, data);
+  }
+  static created(message = 'The request succeeded', data = {}) {
+    return new ApiSuccess(201, message, data);
+  }
+  static accepted(message = 'The request has been received but not yet acted upon.', data = {}) {
+    return new ApiSuccess(202, message, data);
+  }
+  static noAuthoritative(message = 'No Authoritative', data = {}) {
+    return new ApiSuccess(203, message, data);
+  }
+  static noContent(
+    message = 'There is no content to send for this request, but the headers are useful.',
+    data = {}
+  ) {
+    return new ApiSuccess(204, message, data);
+  }
+  static coustomSuccess(statusCode, message = 'Success', data = {}) {
+    return new ApiSuccess(statusCode, message, data);
+  }
 }
 
 export default ApiSuccess;
